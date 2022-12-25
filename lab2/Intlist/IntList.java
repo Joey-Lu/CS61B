51bbfab3309e7c5d8709926fcdf0ee72555952a0
joey <joey.lu.au@gmail.com>
2022-12-25 11:29:26 +1100

    hanlled null arguments

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,8 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if(A == null) return B;
+
         IntList p = A;
         while(p.rest != null){
             p = p.rest;
@@ -94,6 +96,8 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
+        if(A == null) return B;
+
         IntList head1 = new IntList(A.first, A.rest);
         IntList curr1 = head1;
         while(curr1.rest != null){
